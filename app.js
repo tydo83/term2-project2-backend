da211b8e051e763c8b1228c5b0820410dc7ef3cd
Timothy Do <tydo83@gmail.com>
2021-04-27 15:04:44 -0400

    updated app.js

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require("mongoose")
+var cors = require("cors")
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MONGO DB CONNECTED")
+}).catch((err) => {
+  console.log(err)
+})
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users/usersRouter');
 
 var app = express();
 
@@ -13,6 +26,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
